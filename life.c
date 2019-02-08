b191e89845aedb4185b6419309a8662c15c618ff
Rand3rino <37269917+Rand3rino@users.noreply.github.com>
2019-02-08 15:55:45 -0500

    Add files via upload

---
+++ life.c
@@ -0,0 +1,36 @@
+#include "life.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * get_grid creates new memory for a "grid".
+ * x is the height and y is the width.
+ */
+char** get_grid(int x, int y){
+
+}
+
+/*
+ * print_grid attempts to print an x height
+ * by y width grid stored at the location
+ * provided by grid
+ */
+void print_grid(int x, int y, char** grid){
+
+}
+
+/*
+ * mutate takes a grid and mutates that grid
+ * according to Conway's rules.  A new grid
+ * is returned.
+ */
+char** mutate(int x, int y, char** grid){
+
+}
+
+/* get_neighbors is a helper method that returns
+ * the number of live neighbors a cell has.
+ */
+int get_neighbors(int i, int j, int x, int y, char** grid){
+
+}
